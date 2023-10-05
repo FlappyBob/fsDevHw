866ae2fc8e2425bd4af0cbb73f72355cb6a28c33
FlappyBob <flappyyst@outlook.com>
2023-10-05 10:46:44 -0400

    begin step 2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,15 +1,33 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const course = 'Half Stack application development'
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
 
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
+
+    </div>
+  )
+}
+
+const Header = () => {
   return (
     <div>
       <h1>{course}</h1>
+    </div>
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
       <p>
         {part1} {exercises1}
       </p>
@@ -19,6 +37,13 @@ const App = () => {
       <p>
         {part3} {exercises3}
       </p>
+    </div>
+  )
+}
+
+const Total = () => {
+  return (
+    <div>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
